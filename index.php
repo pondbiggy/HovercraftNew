b3c07297f4ce0089d20ab4895c1db76f8ae8f9fe
pondbiggy <73007016+pondbiggy@users.noreply.github.com>
2020-12-28 18:10:05 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <!doctype html>
 <html lang="th">
 <head>
-    <title>WebTech Bootstrap</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <!-- Popper JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hovercraft</title>
-
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+    width: 100%;
+    height: 100%;
+  }
+  </style>
 
 
 
@@ -25,28 +25,49 @@
 
 
 <body>
-<div id="header-secondary-outer" data-full-width="true">
-		<div class="container">
-			<nav>
-				<ul id="social">
-					<li><a target="_blank" href="https://twitter.com/cssnectar"><i class="icon-twitter"></i> </a></li> 						  
-                    <li><a target="_blank" href="https://www.facebook.com/cssnectar"><i class="icon-facebook"></i> </a></li> 												 
-                    <li><a target="_blank" href="http://www.pinterest.com/cssnectar/css-web-gallery-cssnectarcom/"><i class="icon-pinterest"></i> </a></li> 																		 
-                    <li><a target="_blank" href="http://cssnectar.tumblr.com/"><i class="icon-tumblr"></i> </a></li> 												 
-                    <li><a target="_blank" href="https://cssnectar.com/css-gallery-inspiration/feed/"><i class="icon-rss"></i> </a></li> 																		 
-                    <li><a target="_blank" href="https://plus.google.com/115143348319980547079"><i class="icon-google-plus"></i> </a></li> 																																									
-                </ul>
-								
-				<ul class="sf-menu">	
-				   	<li id="menu-item-14273" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-14273"><a href="https://cssnectar.com/css-gallery/nominees/">NOMINEES</a></li>
-                    <li id="menu-item-14274" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-14274"><a href="https://cssnectar.com/css-gallery/winners/">WINNERS</a></li>
-                    <li id="menu-item-13773" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-13773"><a href="https://cssnectar.com/get-ribbon/">GET YOUR RIBBON</a></li>
-                    <li id="menu-item-45042" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-45042"><a href="https://cssnectar.com/contact-us/">CONTACT</a></li>
-				</ul>
-								
-			</nav>
-		</div>
-	</div>
+    <div id="demo" class="carousel slide" data-ride="carousel">
+        <ul class="carousel-indicators">
+            <li data-target="#demo" data-slide-to="0" class="active"></li>
+            <li data-target="#demo" data-slide-to="1"></li>
+            <li data-target="#demo" data-slide-to="2"></li>
+        </ul>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="p11.jpg" alt="Ball" width="1100" height="500">
+            <div class="carousel-caption">
+                <h3>Ball</h3>
+                <p>We had such a great time in LA!</p>
+            </div>   
+        </div>
+        <div class="carousel-item">
+            <img src="p22.jpg" alt="Bee" width="1100" height="500">
+            <div class="carousel-caption">
+                <h3>Bee</h3>
+                <p>Thank you, Chicago!</p>
+            </div>   
+        </div>
+        <div class="carousel-item">
+            <img src="p33.jpg" alt="Jib" width="1100" height="500">
+            <div class="carousel-caption">
+                <h3>Jib</h3>
+                <p>We love the Big Apple!</p>
+            </div>   
+        </div>
+        <div class="carousel-item">
+            <img src="p44.jpg" alt="Phat" width="1100" height="500">
+            <div class="carousel-caption">
+                <h3>Phai</h3>
+                <p>We love the Big Apple!</p>
+            </div>   
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+    </a>
+    </div>
 
 
 </body>
