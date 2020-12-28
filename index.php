4845fd239fd47967b17da524bf3cfeec8845b230
pondbiggy <73007016+pondbiggy@users.noreply.github.com>
2020-12-28 18:43:17 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
   <h2>Card Image</h2>
   <p>Image at the top (card-img-top):</p>
   <div class="card" style="width:400px">
-    <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
+    <img class="card-img-top" src="p11.jpg" alt="Card image" style="width:100%">
     <div class="card-body">
       <h4 class="card-title">John Doe</h4>
       <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
