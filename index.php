7c0bb906e4c037521af59ad396589998268f690b
pondbiggy <73007016+pondbiggy@users.noreply.github.com>
2020-12-28 21:36:23 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -149,7 +149,7 @@
 <script>
 $(document).ready(function(){
   // Activate Carousel with a specified interval
-  $("#myCarousel").carousel({interval: 500});
+  $("#myCarousel").carousel({interval: 2000});
         
   // Enable Carousel Indicators
   $(".item1").click(function(){
