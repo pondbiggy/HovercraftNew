20f829cdb4151dacdfdeffcd5fe368b729c1d18b
pondbiggy <73007016+pondbiggy@users.noreply.github.com>
2020-12-28 20:46:54 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -27,11 +27,18 @@
 
 
 <body>
-<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+
+
+<nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
+  <!-- Brand -->
   <a class="navbar-brand" href="#">Navbar</a>
+
+  <!-- Toggler/collapsibe Button -->
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
+
+  <!-- Navbar links -->
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav">
       <li class="nav-item">
@@ -42,10 +49,11 @@
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
-      </li>    
+      </li>
     </ul>
-  </div>  
+  </div>
 </nav>
+
 
 <header class="header">
         <div class="container">
