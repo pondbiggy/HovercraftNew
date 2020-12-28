3899aa43e7e77982582500a39d2e4fc666b6e98c
pondbiggy <73007016+pondbiggy@users.noreply.github.com>
2020-12-28 23:23:08 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
     width: 50%;
     height: 50%;
   }
+  
     body{
            text-align: center ;
            
@@ -202,13 +203,13 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="g1.jpg" alt="First slide">
+      <img class="d-block w-50" src="g1.jpg" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="g1.jpg" alt="Second slide">
+      <img class="d-block w-50" src="g1.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="g1.jpg" alt="Third slide">
+      <img class="d-block w-50" src="g1.jpg" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
