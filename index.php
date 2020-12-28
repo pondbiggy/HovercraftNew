7195f28bcce967b1380b46492fc08116f5c46d0d
pondbiggy <73007016+pondbiggy@users.noreply.github.com>
2020-12-28 17:30:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="th">
+<head>
+    <title>WebTech Bootstrap</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hovercraft</title>
+    
+
+
+
+</head>
+
+
+
+
+<body>
+    <div class = "wrapper ">
+            
+            <nav class="nav-main" id="nav-main">
+                <div class="top">
+                    <div class="header">
+                        <div class="pull-left">
+                            <span class="link-underlined js-nav-menu" data-menu-id="menu-lang">
+                                English
+                            </span>
+                            <span class="bt-lang">            <svg class="ico-svg" viewbox="0 0 14 9.334">
+                    <use xlink:href="https://www.awwwards.com/assets/images/sprite-icons.svg#flag_en"></use>
+                </svg>
+            </span>
+                        </div>
+                        <div class="pull-right">
+                            <div class="bt-close js-close-menu text-uppercase">close</div>
+                        </div>
+                    </div>
+                    <ul class="menu active" data-menu-name="English" data-menu-id="menu-lang" id="menu-main">
+                        <li class="active"><a href="/" class="item">Home</a></li>
+                                        <li><a href="/websites/" class="item">Winners</a></li>
+                                    <li><a href="/websites/nominees/" class="item">Nominees</a></li>
+                        <li><a href="/collections/" class="item">Collections</a></li>
+                        <li><a href="/academy/" class="item new">Academy <span class="badget">NEW</span></a></li>
+                        <li>
+                            <a href="/directory/" class="item">
+                                Professional directory                    <span class="badget-circle lightyellow">2</span>                </a>
+                        </li>
+                        <li>
+                            <a href="/jobs/" class="item">
+                                Jobs &amp; Talent
+                                <span class="badget-circle aquamarine">10</span>                </a>
+                        </li>
+                        <li><a href="/blog/" class="item">Blog</a></li>
+                        <li><a href="/books/" class="item">eBooks &amp; Publications</a></li>
+                                    <li><a href="/jury/2020/" class="item">JURY 2020</a></li>
+                                    <li>
+                            <a href="/conferences" class="item">
+                                Conferences
+                                <i class="first-top">Toronto / Cancelled</i>
+                                <i>San Francisco / Cancelled</i>
+                            </a>
+                        </li>
+                    </ul>
+                    <header id="header">
+    <div class="header-main">
+        <div class="box-left">
+            <div class="item bt-menu js-nav-main" data-menu-id="menu-main">
+                <div class="ico-menu">
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                </div>
+                <span class="has-tablet">MENU</span>
+            </div>
+        </div>
+        <div class="box-right">
+            <div class="item bt-search " id="js-search-container">
+                            <svg class="ico-svg" viewbox="0 0 14 14">
+        <use xlink:href="https://www.awwwards.com/assets/images/sprite-icons.svg#search"></use>
+    </svg>
+
+            </div>
+
+                                <div class="item login">
+        <span>Are you a member?</span> <strong><a class="text-black open_login" href="/login">Register / log in</a></strong>
+    </div>
+
+            
+                         <div class="item has-tablet" id="bt-submit">
+                                    <a href="/submit/" class="button">
+                        <span>SUBMIT YOUR SITE!</span>
+                    </a>
+                            </div>
+                    </div>
+
+</body>
+
+</html>
+
