314525783e457792311a90dfc12cdf4e719f988c
pondbiggy <73007016+pondbiggy@users.noreply.github.com>
2020-12-29 01:42:50 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -334,7 +334,9 @@ $(document).ready(function(){
        <h5>Contact Us</h5>
 
        <p>
-            <span><a data-toggle="tooltip" title="Follow on FacebookGroup" href="https://www.facebook.com/groups/2097980677004926"><i class="fab fa-facebook-square" style="color:rgb(255, 255, 255);font-size:72px"></i></a></span> 
+            <span><a data-toggle="tooltip" title="Follow on CPSU_Group" href="https://www.facebook.com/groups/CPSUTH"><i class="fab fa-facebook-square" style="color:rgb(255, 255, 255);font-size:72px"></i></a></span> 
+            <span><a data-toggle="tooltip" title="Follow on DSSCSU_Group" href="https://www.facebook.com/groups/DSSCSU"><i class="fab fa-facebook-square" style="color:rgb(255, 255, 255);font-size:72px"></i></a></span> 
+            <span><a data-toggle="tooltip" title="Follow on WebTechnology_Group" href="https://www.facebook.com/groups/2097980677004926"><i class="fab fa-facebook-square" style="color:rgb(255, 255, 255);font-size:72px"></i></a></span> 
            
         </p>
 
