be7987921282bdf8f027582b77cf12af6976b7fc
pondbiggy <73007016+pondbiggy@users.noreply.github.com>
2020-12-29 10:15:40 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -65,10 +65,10 @@
   <!-- Brand -->
   <a class="navbar-brand" href="https://hovercraft4.herokuapp.com/"><strong>Hovercraft</strong></a>
 
-  <!-- Toggler/collapsibe Button -->
+  <!-- Toggler/collapsibe Button 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
-  </button>
+  </button> -->
 
   <!-- Navbar links -->
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
@@ -101,6 +101,21 @@
       </div>
     </li>
     </ul>
+    
+  <div class="pos-f-t">
+    <div class="collapse" id="navbarToggleExternalContent">
+      <div class="bg-dark p-4">
+        <h4 class="text-white">Collapsed content</h4>
+        <span class="text-muted">Toggleable via the navbar brand.</span>
+      </div>
+    </div>
+    <nav class="navbar navbar-dark bg-dark">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </nav>
+</div>
+
   </div>
   
 </nav>
