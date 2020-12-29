590758744f2dca767cfbbef280e818106e6b6786
pondbiggy <73007016+pondbiggy@users.noreply.github.com>
2020-12-29 10:03:05 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -311,14 +311,14 @@
      <span><a data-toggle="tooltip" title="Follow on DSSCSU_Group" href="https://www.facebook.com/groups/DSSCSU"><i class="fab fa-facebook-square" style="color:rgb(102, 179, 255);font-size:72px"></i></a></span> 
      <span><a data-toggle="tooltip" title="Follow on WebTechnology_Group" href="https://www.facebook.com/groups/2097980677004926"><i class="fab fa-facebook-square" style="color:rgb(0, 132, 255);font-size:72px"></i></a></span> 
   </p>
-
+  <p class="float-right">
+        <a href="#"><img src="up.png" alt="ball" width="50" height="50"></a>
+  </p>
 <br>
 <br>
 <br>
  
- <p class="float-right">
-        <a href="#"><img src="up.png" alt="ball" width="50" height="50"></a>
-</p>
+ 
     
    
 
