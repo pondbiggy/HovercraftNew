b10b9ed763294861bc1c54425024ee04c0765cc7
pondbiggy <73007016+pondbiggy@users.noreply.github.com>
2020-12-29 10:01:11 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -312,16 +312,16 @@
      <span><a data-toggle="tooltip" title="Follow on WebTechnology_Group" href="https://www.facebook.com/groups/2097980677004926"><i class="fab fa-facebook-square" style="color:rgb(0, 132, 255);font-size:72px"></i></a></span> 
   </p>
 
-
-
-<div id = "onfoot" class="container--fluid" >
+<br>
+<br>
+<br>
  
  <p class="float-right">
         <a href="#"><img src="up.png" alt="ball" width="50" height="50"></a>
 </p>
     
    
-</div> 
+
 <script>
     $(document).ready(function(){
       $('[data-toggle="tooltip"]').tooltip();   
